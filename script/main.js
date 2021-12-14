acc624fda028d6497b343200e8338c5f433a81fe
Саша <92878664+sanyaskuz@users.noreply.github.com>
2021-12-15 01:10:52 +0200

    tesk2

--- script/main.js
+++ script/main.js
@@ -1,4 +1,4 @@
+const name = prompt('Год рождения?')
+alert(2021-name);
 // const name = prompt('Год ваше рождения?')
-// alert(2021-name);
-const name = prompt('Год ваше рождения?')
-alert(name);
+// alert(name);
