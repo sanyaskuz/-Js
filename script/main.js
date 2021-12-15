e7db9f4b8ce3b1fe1824eeeaecaa64b532ca5b05
Саша <92878664+sanyaskuz@users.noreply.github.com>
2021-12-15 16:42:54 +0200

    tesk4

--- script/main.js
+++ script/main.js
@@ -2,5 +2,17 @@
 // alert(2021-name);
 // const name = prompt('Год ваше рождения?')
 // alert(name);
-const name = prompt('Периметр кватрат')
-alert(name*4);
+// const name = prompt('Периметр кватрат')
+// alert(name*4);
+
+
+// Math.pow(name, 2);
+
+
+// function calculateCircumference(radius) {
+//      Math.PI * radius;
+//   }
+  
+//   calculateCircumference(name);
+const name = prompt('Радиус окружности')
+alert(name*name*3,14);
