0aa0dc7f2ee0c628f1071b93bbbeced0314db2c3
Саша <92878664+sanyaskuz@users.noreply.github.com>
2021-12-15 17:30:47 +0200

    tesk6

--- script/main.js
+++ script/main.js
@@ -5,8 +5,13 @@
 // const name = prompt('Периметр кватрат')
 // alert(name*4);
 //   calculateCircumference(name);
-const name = prompt('Имя')
-alert(name);
+const name  = prompt('Сколько Km')
+const tima = prompt('За сколько времени')
+alert(name/tima);
+let message = '5 задание я не сильно смог реалезовть много чего пересмортрел . \n"но это оказалось сложно с временем еще пока"  \n"И вопросы есть по 4  заданию правильно я его сделал или нет  если будет время сможете пожалуйста скинуть в чем проблема что бы я мог это решить и посоветовать как это сделать"';
+alert(message);
+
+
 
 
 // Math.pow(name, 2);
