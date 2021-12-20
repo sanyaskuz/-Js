5919bd9bf46f728e082cad637ca82a453c4ddd9a
Саша <92878664+sanyaskuz@users.noreply.github.com>
2021-12-20 22:10:36 +0200

    lessons2

--- script3/main.js
+++ script3/main.js
@@ -1,8 +1,8 @@
 const  money = prompt('укажите сумму денег')
 const number = prompt('Укажите цену одной шоколадки') 
 
-alert( Math.floor(money/number));
-// я не понял как показать сдачу (
-    // я решил что сдачи не будет )
+alert(Math.floor(money/number));
+alert((money%number));
+
     
 
