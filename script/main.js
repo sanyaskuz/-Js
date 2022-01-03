a10bcc986a487f8a702728849e1e25925ee3f01f
Саша <92878664+sanyaskuz@users.noreply.github.com>
2022-01-03 18:03:08 +0200

    HW-4

--- script/main.js
+++ script/main.js
@@ -1,56 +1,105 @@
 // Задание №1
-// const a =  Number(prompt('Решаем ленейное уравнение ax+b=0;Введите число (a) от 1доInfinity'))
-// let b = Number(prompt('Введите число (b) от 1доInfinity'))
-// b = (- b);
-// console.log(b);
-
-// alert(`x-равно : ${ (b/a)}`);
-// Я нашел такое решени но не знаю на сколько оно правильное 
-
+// let age = prompt('Укажите ваш возраст')
+// if (age <= 2) {
+//     alert ('you child')
+// } else if (age <= 18) {
+//     alert ('You are a teenager')
+// } else if (age <= 60) {
+//     alert ('You are an adult')
+// } else {
+//     alert ('You are a pensioner')
+// }
 
 
 // Задание №2
-// const clock = +prompt('Введите сколько сейчас часов ')
-// const miutes = +prompt('Введите сколько сейчас минут ')
-// let day = 24
-// let dayMiute = 60 
-// day = (24-clock);
-// dayMiute = (60-miutes);
-// console.log(day, dayMiute );
-// alert(`До конца дня осталось : ${(day )}  :  ${ (dayMiute)} `);
-// Я реалезовал это так 
+// let number = prompt('Укажите число от 0-9')
+
+// if (number == 0) {
+//     alert ('Ваш символ )')
+// } else if (number == 9) {
+//     alert ('Ваш символ (')
+// } else if (number == 8) {
+//     alert ('Ваш символ *')
+// } else if (number == 7) {
+//     alert ('Ваш символ &')
+// } else if ( number == 6) {
+//     alert ('Ваш символ ^')
+// } else if (number == 5) {
+//     alert ('Ваш символ %')
+// } else if (number == 4) {
+//     alert ('Ваш символ $')
+// } else if (number == 3) {
+//     alert ('Ваш символ #')
+// } else if (number == 2) {
+//     alert ('Ваш символ @')
+// } else if (number == 1 ) {
+//     alert ('Ваш символ !')
+// } else {
+//     alert ('Число указано не верно')
+// }
+
+
+
 
 
 // Задание №3
-// const number = prompt('Введите трех значное число')
-// let share = 10
-// share = Math.floor(number/10%10);
-// console.log(share);
-// alert(`Ваше втрое число : ${ (share)}`);
-// Я смог сделать эту задачу так
+// let number = +prompt( 'Введите трехзначное число: ');
+// let number1 = parseInt(number / 100);
+// console.log(number1)
+// let number2 = parseInt(number / 10) % 10;
+// console.log(number2)
+// let number3 = number % 10;
+// console.log(number3)
+// if(number1===number2 || number2===number3 || number1===number3)
+// alert('Найдено совпадение цифр');
+// else
+// alert('Совпадений не найдено');
+// я смог это решить так но с помощю подсказки в интернее и не совсме разобрался как 
+// работает parseInt но я понял что оно делает  число целым и даже при строке если строка начинается с числа  но без него оно не работает  
+
+
+
+
+
 
 // Задача №4
+// let year = prompt('Укажите год')
 
-let number = prompt('Введите пяти значное число')
-num = number%10
-number = number - num
-number = number/10
-number = num + String(number)
+// if (year % 4 == 0) {
+//     console.log(year % 4 == 0);
+//     alert ('Этот год высокосный')
+// } else if (year % 100 != 0) {
+//     console.log(year % 100 != 0);
+//     alert ('Этот год не высокосный')
+// } else if (year % 400 ==0) {
+//     alert ('Этот год высокосный')
+// } else {
+//     alert ('Этот год не высокосный')
+// }
 
 
-alert(number);
-// Я не особо понял как это реалезовать если вам не сложно сможите пожалуйса написать в коментах к дз
+
 
 
 // Задача № 5
+let number = prompt('Укажите число')
+let x = 0 ;
+while (number > x){
+    x *= 10;
+    x += number % 10;
+    number = Math.trunc(number /10);
+}
+if (number == x){
+    alert ('Ваше число полиндром')
+} else if (number == Math.trunc(x /10)) {
+    alert ('Ваше число полиндром')
+} else {
+    alert ('Ваше число не полиндром')
+}
 
-// const sum = +prompt('Введите сумму продаж за прошлый месяц')
-// const salary  = 250
-// let interest = (sum / 10)
-// console.log(interest)
-// alert(`Ваша зарплата за этот месяц : = ${(salary+interest)}`)
 
-// реалезовал эту дз так
+
+
 
 
 
