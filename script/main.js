cd22f6f95a2b703baa848eec26dbbd15861077da
Mac <tatanakovtun@MacBook-Air-Mac.local>
2022-05-24 22:44:08 +0300

    HW-21

--- script/main.js
+++ script/main.js
@@ -1,16 +1,22 @@
 // Задания1
-// let a = document.getElementById('text');
-// function addMessage(msg){
-//   a.innerHTML += msg + '<br/>';
-// }
-// Задания2
-let a = document.getElementById('GET');
-
-window.onbeforeunload = function(e){
-  if(a.value.length > 0){
-    let msg = 'Text not saved';
-    e.returnValue = msg;
-    return msg;
+class Comment{
+  constructor(nam, dat, comm){
+    this.name = nam;
+    this.date = dat;
+    this.comment = comm;
   }
-  return null;
-};
+}
+let Com = [];
+let date = new Date;
+btnOK.addEventListener('click', function(){
+  name1.innerText = document.getElementById('InpName').value;
+  comments.innerText = document.getElementById('comment').value;
+  dat1.innerText = date.toLocaleTimeString()+' '+date.toLocaleDateString();
+ let Comen = new Comment(name1.innerText, dat1.innerText, comments.innerText);
+   Com.push(Comen);
+});
+btnShow.addEventListener('click', function(){
+  let newWin = window.open('comment.html');
+  newWin.obj = Com;
+  console.log(newWin);
+});
