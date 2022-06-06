b0dfa0afd769763cc97c0820cb826238343c2c1b
Mac <tatanakovtun@MacBook-Air-Mac.local>
2022-06-06 21:20:56 +0300

    HW-24

--- script/main.js
+++ script/main.js
@@ -1,107 +1,68 @@
 // Задания1
-
-// while(true){
-//   let prom = prompt('Yor Text');
-//   if(!prom) break;
-//   let li = document.createElement('li');
-//   li.innerText = prom;
-//   UL.append(li);
+// let div = document.createElement('div');
+// div.style.width = '100%';
+// div.style.height = '620px';
+// div.style.backgroundColor = 'aqua';
+// document.body.append(div);
+// let div1 = document.createElement('div');
+// div1.style.backgroundColor = 'antiquewhite';
+// div1.style.width = '150px';
+// div1.style.height = '40px';
+// div1.className = 'coords';
+// div.appendChild(div1);
+// div.addEventListener('click', function(e) { 
+//   // div1.innerHTML = ;
+//   console.log(e.buttons);
+//   if(e.button != 1){
+//     div1.innerHTML = 'Левая конпка миши' + 'X =' + e.clientX   + ',' + 'Y =' + e.clientY ;
+//   }else
+//   if(e.button == 2){
+//     div1.innerHTML = 'Правая конпка миши' + 'X =' + e.clientX   + ',' + 'Y =' + e.clientY ;
+//   }else
+//   if(e.button == 4){
+//     div1.innerHTML = 'средняя конпка миши' + 'X =' + e.clientX   + ',' + 'Y =' + e.clientY ;
+//   }
+// });
+// // Задания 2;
+// let input = document.createElement('input');
+// let h3 = document.createElement('h3');
+// h3.innerText = 'Ваше імя';
+// document.body.append(h3);
+// input.type = 'text';
+// input.name = 'Ваше імя';
+// document.body.append(input);
+// input.addEventListener('keypress',function(e){
+//   if ("1234567890".indexOf(e.key) != -1)
+//     e.preventDefault();
+// });
+// Задания 3
+let btn = document.getElementById('btn');
+let oneDiv = document.getElementById('oneDiv');
+let twoDiv = document.getElementById('twoDiv');
+let threeDiv = document.getElementById('threeDiv');
+oneDiv.style.backgroundColor = 'red';
+twoDiv.style.backgroundColor = 'darkgrey';
+threeDiv.style.backgroundColor = 'darkgrey';
+btn.addEventListener('click', trafficLight, false);
+function trafficLight(){
+  if(oneDiv.style.backgroundColor == 'red'){
+      oneDiv.style.backgroundColor = 'darkgrey';
+      twoDiv.style.backgroundColor = 'yellow';
+  }else 
+  if( twoDiv.style.backgroundColor ==  'yellow'){
+    twoDiv.style.backgroundColor = 'darkgrey';
+    console.log(twoDiv.style.backgroundColor);
+    threeDiv.style.backgroundColor = 'green';
+  }else
+  if(threeDiv.style.backgroundColor == 'green'){
+    threeDiv.style.backgroundColor = 'darkgrey';
+    oneDiv.style.backgroundColor = 'red';
+  }
   
  
-// }
-// Задания2
-// let p = document.createElement('p');
-// p.innerHTML = 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Placeat asperiores cupiditate autem nemo harum delectus alias eveniet doloremque quaerat. Quis voluptas harum hic laudantium architecto officiis aliquam asperiores molestias illum voluptatem! Asperiores veniam officiis ab, rerum molestiae reiciendis quaerat. Blanditiis, fugit. Excepturi eligendi, eius incidunt voluptate optio veniam minus laboriosam nobis sapiente odio tenetur porro delectus iure expedita aperiam temporibus deleniti? Saepe consectetur expedita amet ut, at quis suscipit necessitatibus repellendus, iste earum dolore neque porro! Provident distinctio id asperiores adipisci atque maxime, incidunt aliquid nobis expedita doloremque. Mollitia blanditiis placeat quisquam tempora minus temporibus.';
-// p.className = 'p1';
-// p.id = 'p2';
-// document.body.append(p);
-// window.oncontextmenu = function(){
-//   return false;
-// };
-// window.onselectstart = function(){
-//   return false;
-// };
-// Задания3
+  
+};
 
-
-    let div = document.createElement('div');
-    let div2 = document.createElement('div');
-    document.body.appendChild(div);
-    document.body.appendChild(div2);
-    div.style.display = 'Flex';
-    div2.style.display = 'Flex';
-    div.style.marginLeft = '4px';
-    let p = document.createElement('p');
-    let p2 = document.createElement('p');
-    let p3 = document.createElement('p');
-    let p4 = document.createTextNode(' : ');
-    let p5 = document.createElement('p');
-    p5.append(p4);
-    let p6 = document.createTextNode(' : ');
-    let p7 = document.createElement('p');
-    p7.append(p6);
-    p5.style.fontSize = '20px';
-    p7.style.fontSize = '20px';
-    p.style.color = 'red';
-    p.style.fontSize = '20px';
-    p2.style.color = 'blue';
-    p2.style.fontSize = '20px';
-    p3.style.color = 'green';
-    p3.style.fontSize = '20px';
-    p.innerText = 'hh';
-    p2.innerText = 'min';
-    p3.innerText = 'sec';
-    // document.body.style.display = 'flex';
-     let h = div.append(p);
-     div.append(p5);
-     let m = div.append(p2);
-     div.append(p7);
-     let c = div.append(p3);
-     let btn = document.createElement('button');
-    //  btn.type = 'button';
-     btn.style.width = '50px';
-     btn.innerText = 'Start';
-     btn.style.height = '20px';
-     btn.style.backgroundColor = 'antiquewhite';
-     btn.style.color = 'bleck';
-     let btn2 = document.createElement('button');
-    //  btn2.type = 'button';
-     btn2.innerText = 'Stop';
-     btn2.style.width = '50px';
-     btn2.style.height = '20px';
-     btn2.style.marginLeft = '5px';
-     btn2.style.backgroundColor = 'red';
-     div2.appendChild(btn); 
-     div2.appendChild(btn2);
-
-     btn.addEventListener('click',getTime);
-
-     let timer;
-  function getTime(){
-    let date = new Date();
-    let time = [date.getHours(),date.getMinutes(),date.getSeconds()];
-    if(time[0]<10){
-       time[0] = '0' + time[0];
-      }
-    if(time[1]<10){
-      time[1] ='0' + time[1];
-    }
-    if(time[2]<10){ 
-      time[2] ='0' + time[2];
-    }
-    p.innerText = time[0];
-    p2.innerText = time[1];
-    p3.innerText = time[2];
-    console.log(time);
-    timer = setTimeout("getTime()",1000);
-   
-    
-  }
-  btn2.addEventListener('click',endTime);
-  function endTime(){
-    clearInterval(timer);
-  }
- 
 
 
 
